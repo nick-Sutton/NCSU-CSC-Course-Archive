8a03c3837eafbf31cba0c89214067eec6fdca657
nick-Sutton <136986806+nick-Sutton@users.noreply.github.com>
2024-08-02 13:41:19 -0400

    Update TwoRectangles.java

--- Courses/CSC-116/CSC-116-Self-Study/Two-Rectangles/src/TwoRectangles.java
+++ Courses/CSC-116/CSC-116-Self-Study/Two-Rectangles/src/TwoRectangles.java
@@ -1,46 +1,58 @@
-
 /**
  * CSC 116 Self Study - Two Rectangles
  * @author Nicholas Sutton
  * From - Practice-it at Udub
  * 
- * The Magic Square class creates an odd-sized sqaure of numbers.
- * The sqaure contains consecutive numbers, such that the numbers
- * in the rows, columns, and the numers in the diagonals add up
- * to the same sum.
+ * The Two Square class creates an two constants and uses them to print square.
  */
 
  public class TwoRectangles {
 
-    /**  */
+    /** Width of Rectangle */
     private static final int WIDTH = 7;
+
+    /** Height of Rectangle */
     private static final int HEIGHT = 4;
 
-    private TwoRectangles() {
-        
-    }
-
+    /** 
+     * Draw two rectangles based on the value
+     * of the width and height constants
+     */
     public static void drawRectangles() {
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
+
+        // Prints The first square
+        for (int rows = 0; rows < HEIGHT; rows++) {
+            for (int columns = 0; columns < WIDTH; columns++) {
                 System.out.print("*");
             }
+            // moves the print to the next line
             System.out.println();
         }
 
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
+        // Prints the second square
+        for (int rows = 0; rows < HEIGHT; rows++) {
+
+            // prints the whitespace before the second square
+            for (int columns = 0; columns < WIDTH; columns++) {
                 System.out.print(" ");
             }
 
-            for (int j = 0; j < WIDTH; j++) {
+            //Prints the second square
+            for (int columns = 0; columns < WIDTH; columns++) {
                 System.out.print("*");
 
             }
+            // moves the print to the next line
             System.out.println();
         }
     }
+
+    /** 
+     * Program entry point.
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
+        // Draw the rectangles
         drawRectangles();
     }
 
