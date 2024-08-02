7c9f63cb30e423a7a26b2eb9b18b235f7b18ec73
nick-Sutton <136986806+nick-Sutton@users.noreply.github.com>
2024-08-02 14:14:40 -0400

    add point class

---
+++ Courses/CSC-116/CSC-116-Self-Study/Line-Class/src/Line.java
@@ -0,0 +1,27 @@
+/**
+ * CSC 116 Self Study - Two Rectangles
+ * @author Nicholas Sutton
+ * From - Practice-it at Udub
+ * 
+ * The Two Square class creates an two constants and uses them to print square.
+ */
+
+public class Line {
+
+    public line(Point p1, Point p2) {
+
+
+    }
+
+    public Point getP1() {
+
+    }
+
+    public Point getP2() {
+
+    }
+
+    public String toString() {
+        return "d";
+    }
+}
